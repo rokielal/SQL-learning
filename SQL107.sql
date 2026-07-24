7bb148739ccc95430b8ee038a32bfea2a01d0e86
rokielal <manoranjangupta94@gmail.com>
2026-07-24 09:53:14 +0530

    leanred TOP N

---
+++ SQL107.sql
@@ -0,0 +1,9 @@
+-- TOP N
+
+select * from dbo.Employees
+
+select top 2 * from dbo.Employees
+
+select top 2 FirstName, LastName from dbo.Employees
+
+select top 4 FirstName, Department,Salary from dbo.Employees
